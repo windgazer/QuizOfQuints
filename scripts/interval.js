470a041719fbcd588da20bb54b3cb1fb66ee1972
Martin Reurings <Martin.Reurings@spilgames.com>
2012-09-30 10:45:13 +0200

    Decided to go for single 'db'

--- scripts/interval.js
+++ scripts/interval.js
@@ -24,37 +24,19 @@ var Interval = (function() {
 
 	var major2minor = new Array(
 
-		{ key:"C",  value:"a",  pos:1  },
-		{ key:"G",  value:"e",  pos:2  },
-		{ key:"D",  value:"b",  pos:3  },
-		{ key:"A",  value:"f#", pos:4  },
-		{ key:"E",  value:"c#", pos:5  },
-		{ key:"B",  value:"g#", pos:6  },
-		{ key:"F#", value:"d#", pos:7  },
-		{ key:"Gb", value:"eb", pos:7  },
-		{ key:"Db", value:"bb", pos:8  },
-		{ key:"Ab", value:"f",  pos:9  },
-		{ key:"Eb", value:"c",  pos:10 },
-		{ key:"Bb", value:"g",  pos:11 },
-		{ key:"F",  value:"d",  pos:12 }
-
-	);
-
-	var minor2signature = new Array(
-
-		{ key:"a",  value:"0",  pos:1  },
-		{ key:"e",  value:"1#", pos:2  },
-		{ key:"b",  value:"2#", pos:3  },
-		{ key:"f#", value:"3#", pos:4  },
-		{ key:"c#", value:"4#", pos:5  },
-		{ key:"g#", value:"5#", pos:6  },
-		{ key:"d#", value:"6#", pos:7  },
-		{ key:"eb", value:"6b", pos:7  },
-		{ key:"bb", value:"5b", pos:8  },
-		{ key:"f",  value:"4b", pos:9  },
-		{ key:"c",  value:"3b", pos:10 },
-		{ key:"g",  value:"2b", pos:11 },
-		{ key:"d",  value:"1b", pos:12 }
+		{ key:"C",  value:"a",  sig:"0",  pos:1  },
+		{ key:"G",  value:"e",  sig:"1#", pos:2  },
+		{ key:"D",  value:"b",  sig:"2#", pos:3  },
+		{ key:"A",  value:"f#", sig:"3#", pos:4  },
+		{ key:"E",  value:"c#", sig:"4#", pos:5  },
+		{ key:"B",  value:"g#", sig:"5#", pos:6  },
+		{ key:"F#", value:"d#", sig:"6#", pos:7  },
+		{ key:"Gb", value:"eb", sig:"6b", pos:7  },
+		{ key:"Db", value:"bb", sig:"5b", pos:8  },
+		{ key:"Ab", value:"f",  sig:"4b", pos:9  },
+		{ key:"Eb", value:"c",  sig:"3b", pos:10 },
+		{ key:"Bb", value:"g",  sig:"2b", pos:11 },
+		{ key:"F",  value:"d",  sig:"1b", pos:12 }
 
 	);
 
@@ -101,6 +83,12 @@ var Interval = (function() {
 		return getObjectByFieldValue( parseInt( position, 10 ), "pos" );
 
 	}
+
+	function getValueBySignature( sig ) {
+
+		return getObjectByFieldValue( sig, "sig" );
+
+	};
 
 	return {
 		
